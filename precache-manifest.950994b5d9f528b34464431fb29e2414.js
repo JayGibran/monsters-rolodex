a5fc6758752107fc568ce1037ee6ca1bdfb0aabb
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-09-01 20:33:04 +0100

    Updates

---
+++ precache-manifest.950994b5d9f528b34464431fb29e2414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cde07fa7dcdf1fc1138484384944ed1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c974f77e748131c734c",
+    "url": "/monsters-rolodex/static/css/main.56cd215e.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "6c974f77e748131c734c",
+    "url": "/monsters-rolodex/static/js/main.70e346cf.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
